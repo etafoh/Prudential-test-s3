f3a79e9336c00689acc32b744945428904ea73c4
Rene-Ngong-N <106627399+Rene-Ngong-N@users.noreply.github.com>
2022-09-08 14:08:53 -0500

    Update createS3bucket.py

--- createS3bucket.py
+++ createS3bucket.py
@@ -1,14 +1,9 @@
 import boto3
 import os
-
 os.environ['AWS_PROFILE'] = "default"
 os.environ['AWS_DEFAULT_REGION'] = "us-east-1"
-
-
 sess= boto3.Session(region_name='us-east-1')
-
-
- s3client = sess.client('s3')
+s3client = sess.client('s3')
 # s3 = boto3.resource('s3',
 #          aws_access_key_id=ACCESS_ID,
 #          aws_secret_access_key= ACCESS_KEY)
@@ -19,3 +14,12 @@ s3_location={
 }
 
 s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+
+# import boto3
+# sess= boto3.Session(region_name='us-east-2')
+# s3client = sess.client('s3')
+# bucket_name='prudential-s3'
+# s3_location={
+#     'LocationConstraint': 'us-east-2'
+# }
+# s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
