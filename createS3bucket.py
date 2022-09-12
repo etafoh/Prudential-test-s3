6996250fc22fb32279b56977ada603b6c3b58fbc
etafoh <33801223+etafoh@users.noreply.github.com>
2022-09-12 04:39:46 +0100

    Update createS3bucket.py

--- createS3bucket.py
+++ createS3bucket.py
@@ -1,9 +1,9 @@
 import boto3
 
-sess= boto3.Session(region_name='us-east-2')
+sess= boto3.Session(region_name='us-east-1')
 s3client = sess.client('s3')
 bucket_name='big-thing-happen-big1'
 s3_location={
-    'LocationConstraint': 'us-east-2'
+    'LocationConstraint': 'us-east-1'
 }
 s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
